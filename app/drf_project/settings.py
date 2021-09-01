06e7247b57c84642493ef7f27569657f0114fc64
Jesse <taochenlei@gmail.com>
2021-09-01 16:10:54 +1000

    docker-compose up -d

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -127,3 +127,15 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'movies.CustomUser'
+
+import os
+
+# app/drf_project/settings.py
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
