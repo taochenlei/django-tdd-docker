4739be4912784e78345f6763cf312b6fbc526d08
Jesse <taochenlei@gmail.com>
2021-09-02 14:54:50 +1000

    change for os.environ to fix test errors

--- app/conftest.py
+++ app/conftest.py
@@ -2,6 +2,14 @@ import os
 from django.conf import settings
 import pytest
 
+
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
 
 
@@ -9,11 +17,18 @@ DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
 def django_db_setup():
     settings.DATABASES["default"] = {
         "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
-        "HOST": os.environ["DB_TEST_HOST"],
-        "NAME": os.environ["DB_TEST_NAME"],
-        "PORT": os.environ["DB_TEST_PORT"],
-        "USER": os.environ["DB_TEST_USER"],
-        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+        # "HOST": os.environ["DB_TEST_HOST"],
+        # "NAME": os.environ["DB_TEST_NAME"],
+        # "PORT": os.environ["DB_TEST_PORT"],
+        # "USER": os.environ["DB_TEST_USER"],
+        # "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+
     }
 
 
