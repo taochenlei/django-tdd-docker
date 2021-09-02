8577063eeb709956cff5a0fba9226ff5c9502825
Jesse <taochenlei@gmail.com>
2021-09-02 12:39:46 +1000

    @pytest

---
+++ app/conftest.py
@@ -0,0 +1,24 @@
+import os
+from django.conf import settings
+import pytest
+
+DEFAULT_ENGINE = "django.db.backends.postgresql_psycopg2"
+
+
+@pytest.fixture(scope="session")
+def django_db_setup():
+    settings.DATABASES["default"] = {
+        "ENGINE": os.environ.get("DB_TEST_ENGINE", DEFAULT_ENGINE),
+        "HOST": os.environ["DB_TEST_HOST"],
+        "NAME": os.environ["DB_TEST_NAME"],
+        "PORT": os.environ["DB_TEST_PORT"],
+        "USER": os.environ["DB_TEST_USER"],
+        "PASSWORD": os.environ["DB_TEST_PASSWORD"],
+    }
+
+
+@pytest.fixture(scope="module")
+def foo():
+    # set up code
+    yield "bar"
+    # tear down code
