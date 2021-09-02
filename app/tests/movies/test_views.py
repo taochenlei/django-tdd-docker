6aebe8125e2f224ab062cdfb4bbf3b46d320f760
Jesse <taochenlei@gmail.com>
2021-09-02 15:13:49 +1000

    test_views.py empty+wrong json & httpie

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -24,3 +24,37 @@ def test_add_movie(client):
 
     movies = Movie.objects.all()
     assert len(movies) == 1
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json_keys(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big Lebowski",
+            "genre": "comedy",
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
