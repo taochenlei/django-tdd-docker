92d49065ad78e8a4bef2c9778e6f5505eb77f7f6
Jesse <taochenlei@gmail.com>
2021-09-03 10:10:26 +1000

    disable the Browsable API in production

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -148,3 +148,10 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+if not DEBUG:
+    REST_FRAMEWORK = {
+        "DEFAULT_RENDERER_CLASSES": (
+            "rest_framework.renderers.JSONRenderer",
+        )
+    }
